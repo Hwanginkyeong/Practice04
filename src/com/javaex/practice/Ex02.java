2972ae675a979b881bbf0045b3a2c65403b6c7a1
hwanginkyeong <inke0831@naver.com>
2021-11-19 16:56:10 +0900

    2nd

---
+++ src/com/javaex/practice/Ex02.java
@@ -0,0 +1,22 @@
+package com.javaex.practice;
+
+import java.util.Scanner;
+
+public class Ex02 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double[] i = new double[3]; 
+		
+		//for(i=0; i)
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/com/javaex/practice/Ex03.java
@@ -0,0 +1,11 @@
+package com.javaex.practice;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		//int[ intA = { 3, 6, 9};
+
+	}
+
+}
