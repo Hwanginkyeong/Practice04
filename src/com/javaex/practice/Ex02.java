6c0385a98a765655ad5336503ce1edb8488dc9a8
hwanginkyeong <inke0831@naver.com>
2021-11-29 00:03:42 +0900

    1st

--- src/com/javaex/practice/Ex02.java
+++ src/com/javaex/practice/Ex02.java
@@ -7,8 +7,13 @@ public class Ex02 {
 		double[] num = new double[] {1.2, 3.3, 6.7}; //3개의 룸을 만들고 값을 넣음
 		
 		for(int i = 0; i<num.length; i++ ) {
-			System.out.println(num[i]);
-			
+			if(i==0) {
+				System.out.println(num[2]);
+			}else if(i==1) {
+				System.out.println(num[1]);	
+			}else if(i==2) {
+				System.out.println(num[0]);	
+			}
 		}
 		
 		
