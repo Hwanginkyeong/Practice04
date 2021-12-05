83478a9678ba6adeb429173874792777b384586e
hwanginkyeong <inke0831@naver.com>
2021-12-05 23:30:22 +0900

    1st

--- src/com/javaex/practice/Ex04.java
+++ src/com/javaex/practice/Ex04.java
@@ -2,15 +2,24 @@ package com.javaex.practice;
 
 public class Ex04 {
 
+	private static int count;
+
 	public static void main(String[] args) {
 		
 		int[] data = {1, 3, 5, 8, 9, 11, 15, 19, 18, 20, 30, 33, 31};
+		int result= 0;
 		
 		for(int i=0; i<data.length; i++) {
-			if(i%3==0) {
-				System.out.println();
+			if(data[i]%3==0) {
+				count++;
 			}
-		}
+		}System.out.println("주어진 배열에서 3의 배수의 개수 => "+count);
+		
+		for(int i=0; i<data.length; i++) {
+			if(data[i]%3==0) {
+				result = result + data[i];
+			}
+		}System.out.println("주어진 배열에서 3의 배수의 합 => "+result);
 
 	}
 
--- src/com/javaex/practice/Ex05.java
+++ src/com/javaex/practice/Ex05.java
@@ -6,7 +6,7 @@ public class Ex05 {
 
 	public static void main(String[] args) {
 		
-		Scanner sc = sc.nextInt()
+		//Scanner sc = sc.nextInt()
 				
 				
 
